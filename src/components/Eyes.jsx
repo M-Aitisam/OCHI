7d50f7e4c22e3991a3671cb9eaa3278e40c6c1a0
M-Aitisam <aitisamahmed@24gmail.com>
2024-04-30 01:50:52 +0500

    Create eye of this project ochi using the useEffect variable  before this creating half website of the project

--- src/components/Eyes.jsx
+++ src/components/Eyes.jsx
@@ -1,6 +1,24 @@
-import React from "react";
+import React , { useEffect, useRef, useState } from "react";
 
-function Eyes() {
+
+
+  function Eyes() {
+
+    const[rotate, setRotate]=useState(0);
+    
+    useEffect(() => {
+      window.addEventListener("mousemove", (e) => {
+        let mouseX = e.clientX;
+        let mouseY = e.clientY;
+      
+        let deltaX = mouseX - window.innerWidth / 2;
+        let deltaY = mouseY - window.innerHeight / 2;
+      
+       var angle = Math.atan2(deltaY, deltaX) * (180 / Math.PI);
+        setRotate(angle-180);
+      })
+    })
+    
   return (
     <div className=" eyes w-full h-screen  overflow-hidden">
       <div
@@ -9,17 +27,24 @@ function Eyes() {
       >
         <div className="absolute  top-[40%] flex  left-[50%] gap-10 -translate-x-1/2 -translate-y-1/2  min-h-[30vh] ">
           <div className=" flex items-center justify-center w-[15vw] h-[15vw]  rounded-full bg-white">
-            <div className="w-[12vw] flex items-center justify-center h-[12vw] rounded-full bg-black">
-              <div className="line w-full h-10 rotate-12 ">
-                <div className="w-[2.5vw] h-[2.5vw] rounded-full bg-white"></div>
+            <div className="w-[10vw] relative h-[10vw] rounded-full bg-black">
+            <div className="t flex items-center  p-[4vw] text-white justify-center font-bold text-2xl "> PLAY</div>
+              <div style={{transform: `translate(-50%, -50%) rotate(${rotate}deg)`}} className="line absolute top-1/2 left-1/2 -translate-x-[50%]  -translate-y-1/2 w-full h-10 rotate-12 ">
+                <div className="w-[1.5vw] h-[1.5vw] rounded-full bg-white"></div>
               </div>
             </div>
           </div>
           <div className=" flex items-center justify-center w-[15vw] h-[15vw]  rounded-full bg-white">
-            <div className="w-[12vw] flex items-center justify-center h-[12vw] rounded-full bg-black">
-              <div className="line w-full h-10 rotate-12 ">
-                <div className="w-[2.5vw] h-[2.5vw] rounded-full bg-white"></div>
-              </div>
+          
+            <div className="w-[10vw] relative h-[10vw]   rounded-full bg-black"> 
+            <div className="t flex items-center  p-[4vw] text-white justify-center font-bold text-2xl "> PLAY</div>
+            <div style={{transform: `translate(-50%, -50%) rotate(${rotate}deg)`}} className="line   absolute top-1/2 left-1/2 -translate-x-
+[50%]  -translate-y-1/2 w-full h-10 rotate-12 ">
+    <div className="w-[1.5vw] h-[1.5vw] rounded-full bg-white"> 
+     </div>
+  </div>
+             
+             
             </div>
           </div>
         </div>
